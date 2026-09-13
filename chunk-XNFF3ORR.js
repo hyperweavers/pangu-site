ba79e29d0bce14cf814f46815f840cdfca962f35
hyper-weavers <info@hyperweavers.com>
2026-09-13 18:25:56 +0000

    Deploy pangu App 🚀

---
+++ chunk-XNFF3ORR.js
@@ -0,0 +1,1 @@
+import{Ba as c,g as o,ia as s,pc as t,ra as i,ta as u}from"./chunk-RIRV354U.js";function O(a,d){let n=d?.injector??s(i),e=new o(1),l=c(()=>{let r;try{r=a()}catch(b){t(()=>e.error(b));return}t(()=>e.next(r))},{injector:n,manualCleanup:!0});return n.get(u).onDestroy(()=>{l.destroy(),e.complete()}),e.asObservable()}export{O as a};
